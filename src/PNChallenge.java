2ee211705191cc7aebe960feb7646c1adbd881dd
Shifera <shiferawregassa@gmail.com>
2019-10-24 16:31:13 -0400

    second commit

--- src/PNChallenge.java
+++ src/PNChallenge.java
@@ -1,23 +1,33 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class PNChallenge {
-    public static void main (String[]args) {
-        int ok;
-        boolean isPrime=true;
+    public static void main(String[] args) {
+        Random r = new Random();
+        int x = 1 + r.nextInt(250);
+        System.out.println(x);
+
+                int ok;
+        boolean isPrime = true;
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Enter any number");
+        System.out.println("Enter any number between 1 and 250");
+        System.out.println("Enter the random number");
         int num = keyboard.nextInt();
         keyboard.close();
-        for(int i=2;i<=num/2;i++) {
-            ok=num%i;
-            if(ok==0) {
-                isPrime=false;
+        for (int i = 2; i <= num / 2; i++) {
+            ok = num % i;
+            if (ok == 0) {
+                isPrime = false;
                 break;
             }
         }
-        if(isPrime)
-            System.out.println(num + "is a prime number");
+        if (isPrime)
+            System.out.println(num + " is a prime number");
         else
-            System.out.println(num + "is not a prime number");
+            System.out.println(num + " is not a prime number");
     }
 }
+
+
+
+
